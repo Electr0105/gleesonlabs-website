0a1613c1a3de70a6e5d4420ccb059da4c1cdd5a3
Jackson <jackson.gleeson@amtek.com.au>
2025-08-29 16:41:44 +1000

    styling

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("public");
+  
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site"
+      output: "_site",
     }
   };
 };
---
+++ src/js/scripts.js
@@ -0,0 +1,17 @@
+  const countVisitors = async () => {
+    try {
+      const response = await fetch('https://resume-functions.azurewebsites.net/api/visitorcount');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      document.getElementById("visitorcount").textContent = 
+        "Number of site visitors: " + (data ?? "Unavailable");
+
+    } catch (error) {
+      console.error("Failed to fetch visitor count:", error);
+      document.getElementById("visitorcount").textContent = 
+        "Number of site visitors: Unavailable";
+    }
+  };
